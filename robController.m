5f8a51e1e4ba7afdaf4a97094c555f8c66b996fe
nickbobescobar <41488955+nickbobescobar@users.noreply.github.com>
2020-04-29 16:38:07 -0500

    Add files via upload
    fixed controller torques

---
+++ robController.m
@@ -0,0 +1,50 @@
+function [ tau ] = robController( trajectory, Theta, Theta_dot, t , rob )
+% MECH 498/598 - Intro to Robotics - Spring 2016
+% Lab 3
+% Solutions by Craig McDonald
+%
+%    DESCRIPTION - This function sends command torques in the vector tau to move the
+%    robot. The controller determines tau based on the desired position and
+%    velocity and the actual position and velocity. Desired position and
+%    velocity are obtained from trajectory and the current time t. The
+%    actual position is Theta, and the actual velocity is Theta_dot.
+%    Calculate the torques needed to add gravity compensation using the
+%    robot parameters from the structure rob.
+%
+
+% Robot Parameters from rob
+g = rob.parameters.g;
+b = rob.parameters.b;
+m1 = rob.parameters.m1;
+m2 = rob.parameters.m2;
+m3 = rob.parameters.m3;
+m4 = rob.parameters.m4;
+l1 = rob.parameters.l1;
+l2 = rob.parameters.l2;
+l3 = rob.parameters.l3;
+I1 = rob.parameters.I1;
+I2 = rob.parameters.I2;
+J1 = rob.parameters.J1;
+J2 = rob.parameters.J2;
+J3 = rob.parameters.J3;
+    
+% Gravity Compensation Vector
+G = [m1*g*cos(Theta(1)); m2*g*cos(Theta(2)); (m3+m4)*g*cos(Theta(3))]; %[3x1] vector
+
+% Trajectory interpolation (DO NOT CHANGE)
+Theta_ref = zeros(3,1);
+Theta_dot_ref = zeros(3,1);
+for i = 1:3
+    Theta_ref(i) = interp1(trajectory(1,:),trajectory(i+1,:),t);
+    Theta_dot_ref(i) = interp1(trajectory(1,:),trajectory(i+4,:),t);
+end
+
+% Gravity Compensation Control
+
+K_p = 0*[1, 1, 1]; % Proportional gain matrix containing gains K_p1 to K_p3
+K_v = 0*[1, 1, 1]; % Derivative gain matrix containing gains K_v1 to K_v3
+
+tau = -(diag(K_p)*(Theta - Theta_ref) + diag(K_v)*(Theta_dot - Theta_dot_ref)) + G; % control input (torque)
+
+end
+
